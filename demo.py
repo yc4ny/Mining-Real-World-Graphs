a6b1175e0ce2b1dc301a29bfcd3e5c0f53f7ad3d
yc4ny <yhugestar@snu.ac.kr>
2022-12-04 23:56:53 +0900

    Added getGraphName function

--- demo.py
+++ demo.py
@@ -2,18 +2,20 @@ import argparse
 import os
 import subprocess
 
-def getGraphName(path):
+def getGraphName(input_directory):
     input_dir = args.input 
     file_path = input_dir.split('/')
     filename = file_path[len(file_path)-1]
     filename = filename.replace('.csv', '')
+    return filename
 
 if __name__ == "__main__":
+    # Parse command line arguments  
     parser = argparse.ArgumentParser(description = "Age Classification")
     parser.add_argument('--input', type = str, default = 'datasets/facebook_clean_data/artist_edges.csv', help = 'output from community detection')
     args = parser.parse_args()
-    
+
+    # Get the graph name for future saving purposes
     filename = getGraphName(args.input)
 
-    print(filename)
     # subprocess.run(["ls", "-l"])
